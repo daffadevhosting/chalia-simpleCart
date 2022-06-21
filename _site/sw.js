7571af905b3e7c4f260fc69ae9b5825629fd6206
“daffadev-inc” <daffadev.inc@gmail.com>
2022-06-21 12:09:04 +0700

    update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,3 @@
----
-layout: none
----
 importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn/workbox/workbox-sw.js');
 
 const { registerRoute } = workbox.routing;
@@ -8,8 +5,8 @@ const { CacheFirst, NetworkFirst, StaleWhileRevalidate } = workbox.strategies;
 const { CacheableResponse } = workbox.cacheableResponse;
 
 workbox.core.setCacheNameDetails({
-  prefix: '{{site.url}}',
-  suffix: '{{ site.time | date: "%Y-%m" }}'
+  prefix: 'http://localhost:4000',
+  suffix: '2022-06'
 });
 
 registerRoute(
@@ -28,13 +25,10 @@ registerRoute(
 )
 
 workbox.precaching.precacheAndRoute([
-  {% for post in site.posts limit:14 -%}
-  { url: '{{ post.url }}', revision: '{{ post.date | date: "%Y-%m-%d"}}' },
-  {% endfor -%}
-  { url: '/', revision: '{{ site.time | date: "%Y%m%d%H%M" }}' },
-  { url: '/cart/', revision: '{{ site.time | date: "%Y%m%d%H%M" }}' },
-  { url: '/checkout/', revision: '{{ site.time | date: "%Y%m%d%H%M" }}' },
-  { url: '/css/main.css', revision: '{{ site.time | date: "%Y%m%d%H%M" }}' }
+  { url: '/', revision: '202206211208' },
+  { url: '/cart/', revision: '202206211208' },
+  { url: '/checkout/', revision: '202206211208' },
+  { url: '/css/main.css', revision: '202206211208' }
 ])
 
 registerRoute(
